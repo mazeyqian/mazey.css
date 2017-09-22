fa0a39fdb574b8b1ee66c00a21634dfcd557e2b8
mazey <mazey@mazey.net>
2017-09-22 17:27:21 +0800

    Complete category template by -python2.7.php

--- wp-content/themes/begin/single.php
+++ wp-content/themes/begin/single.php
@@ -4,23 +4,11 @@
     <div id="home-loop">
         <?php
             the_post();
+            $cat = get_the_category($post->ID); /* 分类信息 */
+            $name = $cat[0]->slug;
+            echo $name;
+            get_template_part('container', $name);
         ?>
-                <div class="post-title"><h2>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2></div>
-                <div>
-                    <?php
-                        the_category(',');
-                        the_author();
-                        the_time('Y-m-d H:i:s');
-                        edit_post_link('Edit', 'B', 'A');
-                    ?>
-                </div>
-                <div class="post-content"><?php the_content(); ?></div>
-                <ul>
-                	<li><?php previous_post_link('自定义文本%link'); ?></li>
-                	<li><?php next_post_link('下一篇: %link'); ?></li>
-                </ul>
     </div>
 
     <?php get_sidebar(); ?>
