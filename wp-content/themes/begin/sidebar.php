b093a97816c57d75f06f352187af73af8cdcdd83
mazey <mazey@mazey.net>
2017-09-25 21:15:53 +0800

    Complete

--- wp-content/themes/begin/sidebar.php
+++ wp-content/themes/begin/sidebar.php
@@ -12,3 +12,23 @@
     }
 ?>
 </div>
+<div>
+    <h3>随机文章</h3>
+    <div>
+        <?php
+            $args = array(
+                'numberposts' => 5,
+                'category' => '3',
+                'orderby' => 'rand'
+            );
+            $rand_posts = get_posts($args);
+        ?>
+        <ul>
+        <?php foreach($rand_posts as $post) {
+            setup_postdata( $post );
+            ?>
+        	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php } ?>
+        </ul>
+    </div>
+</div>
