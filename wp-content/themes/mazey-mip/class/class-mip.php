d6df5f6f8e3d3dbadb417520aa6d8cacf1277498
mazey <mazey@mazey.net>
2017-09-28 16:41:01 +0800

    Solve canonical

--- wp-content/themes/mazey-mip/class/class-mip.php
+++ wp-content/themes/mazey-mip/class/class-mip.php
@@ -3,12 +3,18 @@
 class Class_MIP {
     /* 获取当前地址 */
     private function return_current_url() {
+        $current_url = get_bloginfo('url');
         if(is_home()) {
-            return bloginfo('url');
-        } else {
-            return the_permalink();
+            $current_url = get_bloginfo('url');
+        } elseif(is_tax() || is_tag() || is_category()) {
+            $query_object = get_queried_object();
+            $current_url = get_term_link($query_object, $query_object->taxonomy);
+        } elseif(is_page() || is_single) {
+            $current_url = get_permalink();
         }
+        return $current_url;
     }
+
     public function print_current_url() {
         echo $this->return_current_url();
     }
