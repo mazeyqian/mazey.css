80cf752a5cf088d7e632dccf9c87185903382396
mazey <mazey@mazey.net>
2017-10-01 01:52:45 +0800

    Remove superfluous code

--- wp-content/themes/mazey-mip/template/part-post-content.php
+++ wp-content/themes/mazey-mip/template/part-post-content.php
@@ -13,29 +13,25 @@
                         </div>
 
                         <footer>
-                            <?php if(is_single()): ?>
-                            <div class="post-tags">
-                            <?php
+                            <?php if(is_single()):
                                 /* 当前标签 */
                                 //var_dump(get_the_tags());
                                 $listThisTags = get_the_tags();
                                 /* 判断是否存在标签 */
                                 if($listThisTags):
-                                    $listThisTagsID = array();
+                                    echo "<div class='post-tags'>";
                                     $listThisTagsName = array();
                                     $listThisTagsLink = array();
                                     foreach($listThisTags as $detailThisTags):
                                         $listThisTagsLink[] = get_tag_link($detailThisTags->term_id);
                                         $listThisTagsName[] = $detailThisTags->name;
                                     endforeach;
-                                    /* print_r($listThisTagsLink);
-                                    print_r($listThisTagsName); */
                                     for($index = 0, $max = count($listThisTagsLink); $index < $max; $index ++):
                                         echo "<a href='{$listThisTagsLink[$index]}' class='btn btn-defalut'>{$listThisTagsName[$index]}</a>";
                                     endfor;
+                                    echo "</div>";
                                 endif;
                             ?>
-                            </div>
                             <ul class="post-page">
                             	<li><?php previous_post_link('上一篇：%link'); ?></li>
                             	<li><?php next_post_link('下一篇: %link'); ?></li>
