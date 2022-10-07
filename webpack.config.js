d6dac71b9c15a33bbfa625396f4e32befb99da0c
Mazey/后除 <mazey@mazey.net>
2022-10-07 18:17:24 +0800

    chore: add var ENTRY

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const ENTRY = process.env.ENTRY;
+console.log(`ENTRY: ${ENTRY}`);
+
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    [ENTRY]: `./src/${ENTRY}.js`
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'lib'),
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // MiniCssExtractPlugin.loader, // mini-css-extract-plugin's loader only takes the output of css-loader as its input. 
-          // "css-loader",
+          "style-loader",
+          // mini-css-extract-plugin's loader only takes the output of css-loader as its input. 
           {
             loader: MiniCssExtractPlugin.loader,
           },
@@ -24,7 +29,6 @@ module.exports = {
             loader: 'css-loader',
             options: { modules: true },
           },
-          // "style-loader",
           {
             loader: "sass-loader",
             options: {
@@ -41,16 +45,16 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       // filename: `../style.min.css`
-      filename: 'style.css',
+      filename: `${ENTRY}.css`,
     }),
     new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, 'dist/index.html'),
-      template: path.resolve(__dirname, 'src/index.html'),
+      filename: path.resolve(__dirname, `lib/${ENTRY}.html`),
+      template: path.resolve(__dirname, 'src/template/index.html'),
       inject: true,
       chunksSortMode: 'auto'
     }),
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['dist'],
+      cleanOnceBeforeBuildPatterns: ['lib'],
     }),
   ],
 };
