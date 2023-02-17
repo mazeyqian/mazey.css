7234e5ab7e126498fdb8c7944d35ca36c5642211
Mazey Chu <mazey@mazey.net>
2023-02-17 10:33:55 +0800

    feat: optimize the new line

--- src/confluence.js
+++ src/confluence.js
@@ -2,6 +2,8 @@ import './z-style/confluence.scss';
 
 console.log('confluence');
 
+// Area: `.wiki-content`
 if (window.$) {
   $('.wiki-content>p>br').hide();
+  $('.wiki-content td.confluenceTd>a').after("<br />");
 }
