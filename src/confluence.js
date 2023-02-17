62028dc14915dd6114f451a13ad078f741b76808
Mazey Chu <mazey@mazey.net>
2023-02-17 17:17:17 +0800

    style: add comments

--- src/confluence.js
+++ src/confluence.js
@@ -4,6 +4,8 @@ console.log('confluence');
 
 // Area: `.wiki-content`
 if (window.$) {
+  // Format
   $('.wiki-content>p>br').hide();
+  // Table
   $('.wiki-content td.confluenceTd>a').after("<br />");
 }
